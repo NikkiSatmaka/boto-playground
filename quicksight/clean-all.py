3bc9af21cf587668b6c9f3282f34703677aff650
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2025-02-04 13:13:38 +0700

    update clean all

--- quicksight/clean-all.py
+++ quicksight/clean-all.py
@@ -13,6 +13,7 @@ AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_REGION = os.environ["AWS_REGION"]
 AWS_PROFILE = os.environ["AWS_PROFILE"]
 REGION = os.environ["AWS_REGION"]
+AWS_ACCOUNT_ID = boto3.client("sts").get_caller_identity()["Account"]
 
 app = typer.Typer()
 
@@ -20,17 +21,15 @@ app = typer.Typer()
 @app.command()
 def main(region: str):
     session = boto3.Session()
-    sts = session.client("sts")
-    aws_account_id = sts.get_caller_identity()["Account"]
 
-    logger.info(f"AWS Account ID: {aws_account_id}")
+    logger.info(f"AWS Account ID: {AWS_ACCOUNT_ID}")
 
     qs = session.client("quicksight", region_name=region)
 
-    qs_data_sources = qs.list_data_sources(AwsAccountId=aws_account_id)
-    qs_data_sets = qs.list_data_sets(AwsAccountId=aws_account_id)
-    qs_analysis = qs.list_analyses(AwsAccountId=aws_account_id)
-    qs_dashboards = qs.list_dashboards(AwsAccountId=aws_account_id)
+    qs_data_sources = qs.list_data_sources(AwsAccountId=AWS_ACCOUNT_ID)
+    qs_data_sets = qs.list_data_sets(AwsAccountId=AWS_ACCOUNT_ID)
+    qs_analysis = qs.list_analyses(AwsAccountId=AWS_ACCOUNT_ID)
+    qs_dashboards = qs.list_dashboards(AwsAccountId=AWS_ACCOUNT_ID)
 
     def get_keys(data_sources, func_getter, criteria):
         return list(map(func_getter, filter(criteria, data_sources)))
@@ -40,14 +39,14 @@ def main(region: str):
         qs_data_sources["DataSources"], itemgetter("DataSourceId"), lambda x: x
     ):
         logger.debug(f"Deleting Data Source: {i}")
-        qs.delete_data_source(AwsAccountId=aws_account_id, DataSourceId=i)
+        qs.delete_data_source(AwsAccountId=AWS_ACCOUNT_ID, DataSourceId=i)
 
     logger.debug("Deleting Data Sets")
     for i in get_keys(
         qs_data_sets["DataSetSummaries"], itemgetter("DataSetId"), lambda x: x
     ):
         logger.debug(f"Deleting Data Set: {i}")
-        qs.delete_data_set(AwsAccountId=aws_account_id, DataSetId=i)
+        qs.delete_data_set(AwsAccountId=AWS_ACCOUNT_ID, DataSetId=i)
 
     logger.debug("Deleting Analysis")
     for i in get_keys(
@@ -55,7 +54,7 @@ def main(region: str):
     ):
         logger.debug(f"Deleting Analysis: {i}")
         qs.delete_analysis(
-            AwsAccountId=aws_account_id, AnalysisId=i, ForceDeleteWithoutRecovery=True
+            AwsAccountId=AWS_ACCOUNT_ID, AnalysisId=i, ForceDeleteWithoutRecovery=True
         )
 
     logger.debug("Deleting Dashboards")
@@ -63,7 +62,7 @@ def main(region: str):
         qs_dashboards["DashboardSummaryList"], itemgetter("DashboardId"), lambda x: x
     ):
         logger.debug(f"Deleting Dashboard: {i}")
-        qs.delete_dashboard(AwsAccountId=aws_account_id, DashboardId=i)
+        qs.delete_dashboard(AwsAccountId=AWS_ACCOUNT_ID, DashboardId=i)
 
 
 if __name__ == "__main__":
